8cbc4417779be2ea24198cf3f748046461cd4612
Just-Ven <alnurbaisar@gmail.com>
2024-01-19 21:19:41 +0600

    create variables

--- lab 1/comments/comments.py
+++ lab 1/comments/comments.py
@@ -1,1 +1,11 @@
-# Это однострочный комментарий
+# Exercise 1
+# This is a comment
+
+# Exercise 2
+'''
+this
+is a
+multiple
+lined
+comment
+'''
--- lab 1/syntax/syntax.py
+++ lab 1/syntax/syntax.py
@@ -1,2 +1,6 @@
 # Exercise 1
 print('Hello, World!')
+
+# Exerxise 2
+if 5 > 2:
+    print('YES')
