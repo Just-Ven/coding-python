8b425bd4d21d5d1d3741a9e91bf999d488013e04
Just-Ven <alnurbaisar@gmail.com>
2024-01-22 23:26:46 +0600

    added examples for all files

--- lab 1/syntax/syntax.py
+++ lab 1/syntax/syntax.py
@@ -1,6 +1,27 @@
+# Example
+if 5 > 2:
+  print("Five is greater than two!")
+
+
+if 5 > 2:
+ print("Five is greater than two!")
+if 5 > 2:
+        print("Five is greater than two!")
+
+
+x = 5
+y = "Hello, World!"
+
+
+#This is a comment.
+print("Hello, World!")
+
+
+
+
 # Exercise 1
 print('Hello, World!')
 
-# Exerxise 2
+# Exercise 2
 if 5 > 2:
     print('YES')
