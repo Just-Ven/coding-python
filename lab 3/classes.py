71816f0c4a6bc2a661cc6bae98bbb4a949aaebf3
Just-Ven <alnurbaisar@gmail.com>
2024-01-30 15:40:48 +0600

    modified some files

---
+++ lab 3/classes.py
@@ -0,0 +1,23 @@
+# Execise 1
+class MyClass:
+  x = 5
+
+# Execise 2
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+
+# Execise 3
+class MyClass:
+  x = 5
+
+p1 = MyClass()
+
+print(p1.x)
+
+# Exerice 4
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
